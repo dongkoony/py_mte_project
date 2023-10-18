26b48ec0850b5ea4ad3974a37befcaad087c7fda
donghyeon <donghyun4591@gmail.com>
2023-10-18 19:17:31 +0900

    디렉토리 수정

--- backend/utils/ssh_linux.py
+++ backend/utils/ssh_linux.py
@@ -20,7 +20,6 @@ def get_server_info(host, port, username, password=None, key_path=None):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
-    # 서버 연결
     try:
         if key_path:
             private_key = paramiko.RSAKey.from_private_key_file(key_path)
@@ -30,16 +29,13 @@ def get_server_info(host, port, username, password=None, key_path=None):
     except Exception as e:
         raise Exception(f"SSH 연결 오류: {str(e)}")
     
-    # OS 정보 가져오기
     stdin, stdout, stderr = client.exec_command('uname -a')
     os_info = stdout.read().decode().strip()
 
-    # CPU 정보 가져오기
     stdin, stdout, stderr = client.exec_command('lscpu')
     cpu_info = stdout.read().decode().split("\n")
     cpu_details = {line.split(":")[0].strip(): line.split(":")[1].strip() for line in cpu_info if line}
 
-    # Memory 정보 가져오기
     stdin, stdout, stderr = client.exec_command('free -h')
     memory_info = stdout.read().decode().split("\n")[1].split()
     memory_info = {
@@ -48,15 +44,12 @@ def get_server_info(host, port, username, password=None, key_path=None):
         'free': memory_info[3]
     }
 
-    # Disk 정보 가져오기
     stdin, stdout, stderr = client.exec_command('df -h')
     disk_info = stdout.read().decode().split("\n")[1:-1]
     disks_info = [{'filesystem': disk.split()[0], 'size': disk.split()[1], 'used': disk.split()[2], 'available': disk.split()[3]} for disk in disk_info]
 
-    # 연결 종료
     client.close()
 
-    # 결과 반환
     result = {
         'OS': os_info,
         'CPU': cpu_details,
@@ -67,28 +60,25 @@ def get_server_info(host, port, username, password=None, key_path=None):
     return result
 
 def get_terraform_template(host_info):
-    """
-    주어진 호스트 정보를 사용하여 Jinja2 템플릿을 렌더링하고 테라폼 코드를 반환한다.
-    
-    Args:
-    - host_info (dict): 호스트의 OS, CPU 등의 정보가 포함된 딕셔너리
-    
-    Returns:
-    - str: 렌더링된 테라폼 코드
-    """
-    # 템플릿 불러오기
     try:
         template = env.get_template('ec2_template.tf.j2')
     except Exception as e:
         raise Exception(f"템플릿 파일 로딩 중 오류 발생: {str(e)}")
 
-    # 필요한 데이터 추출하기
     cpu_model = host_info.get("CPU", {}).get("Model name", "t2.micro")  # default to t2.micro if not found
     os_name = host_info.get("OS", "").split()[0]  # OS 이름
 
-    # 아래의 코드는 host_info의 내용을 확인하기 위한 코드입니다.
-    # 실제 배포나 실행에서는 제거해야 합니다.
-    print("Host Info:", host_info)
-
-    # 템플릿 적용하고 반환하기
     return template.render(cpu_model=cpu_model, os_name=os_name)
+
+def get_data_from_s3(bucket_name, s3_key):
+    s3 = boto3.client('s3')
+    response = s3.get_object(Bucket=bucket_name, Key=s3_key)
+    json_data = json.loads(response['Body'].read().decode('utf-8'))
+    return json_data
+
+def convert_json_to_terraform(json_data):
+    return get_terraform_template(json_data)
+
+def connect_and_save_to_s3(host, port, username, password=None, key_path=None, bucket_name="mte-project", s3_key="json(terraform)/server_info.json"):
+    server_info = get_server_info(host, port, username, password, key_path)
+    save_data_to_s3(server_info, bucket_name, s3_key)
